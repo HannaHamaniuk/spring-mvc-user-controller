4f47de978d719b04553ffdd0e6f62939f5ed3d0e
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 12:47:18 +0300

    created UserDaoImpl and UserServiceImpl classes

--- src/main/java/by/anne/model/dao/UserDaoImpl.java
+++ src/main/java/by/anne/model/dao/UserDaoImpl.java
@@ -1,12 +1,15 @@
 package by.anne.model.dao;
 
 import by.anne.model.entities.User;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class UserDaoImpl implements UserDao {
     @Override
     public List<User> selectAllUsers() {
-        return null;
+        return new ArrayList<>();
     }
 }
---
+++ src/main/java/by/anne/model/services/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package by.anne.model.services;
+
+import by.anne.model.dao.UserDao;
+import by.anne.model.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserServiceImpl implements UserService {
+    private UserDao dao;
+
+    @Autowired
+    public void setDao(UserDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return dao.selectAllUsers();
+    }
+}
