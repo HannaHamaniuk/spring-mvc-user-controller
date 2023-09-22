f801c1009c775e4ba06a3af5b02a896b6a2f1c93
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 13:48:04 +0300

    added SessionFactory field to UserDaoImpl

--- src/main/java/by/anne/model/dao/UserDaoImpl.java
+++ src/main/java/by/anne/model/dao/UserDaoImpl.java
@@ -1,15 +1,26 @@
 package by.anne.model.dao;
 
 import by.anne.model.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class UserDaoImpl implements UserDao {
+    private SessionFactory factory;
+    @Autowired
+    public void setFactory(SessionFactory factory) {
+        this.factory = factory;
+    }
+
     @Override
     public List<User> selectAllUsers() {
-        return new ArrayList<>();
+
+        try(Session session= factory.openSession()){
+           return  session.createQuery("From User",User.class).list();
+        }
     }
 }
