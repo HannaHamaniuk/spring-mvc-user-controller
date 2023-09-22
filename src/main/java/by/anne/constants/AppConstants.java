5f16e6ddb1980215256a119d8ecc78664386605d
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 17:47:51 +0300

    added several constants in AppConstants

--- src/main/java/by/anne/constants/AppConstants.java
+++ src/main/java/by/anne/constants/AppConstants.java
@@ -6,5 +6,9 @@ public class AppConstants {
     public static final String USER_LIST_ATTR = "userList";
     public static final String USER_PAGE = "users";
     public static final String INDEX_PAGE = "index";
+    public static final String ADD_USER_URL = "/add";
+    public static final String ADD_USER_PAGE= "add";
+    public static final String USER_ATTR = "user";
+    public static final String SAVE_USER_URL = "/save";
 
 }
