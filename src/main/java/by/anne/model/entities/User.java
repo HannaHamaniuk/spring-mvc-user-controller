efd78087244daf6f9c84dc84197f763c8d860575
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 13:24:38 +0300

    added annotation for Spring and Hibernate

--- src/main/java/by/anne/model/entities/User.java
+++ src/main/java/by/anne/model/entities/User.java
@@ -1,6 +1,22 @@
 package by.anne.model.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
+
 public class User {
+    @NonNull
+    @Id//primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int age;
