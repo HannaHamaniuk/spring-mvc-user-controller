6cddf39b9eee3ca8c0419b5134236bb619230397
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 12:47:35 +0300

    created UserController

---
+++ src/main/java/by/anne/controllers/UserController.java
@@ -0,0 +1,22 @@
+package by.anne.controllers;
+
+import by.anne.model.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    private UserService service;
+
+    @Autowired
+    public void setService(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value=)
+    public String all(Model model){
+
+    }
+}
