c4136e2f31c339ba6ff5cad2558fa9e5702027c3
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 18:02:15 +0300

    added new import statements

--- src/main/java/by/anne/controllers/UserController.java
+++ src/main/java/by/anne/controllers/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -20,22 +23,39 @@ public class UserController {
     public void setService(UserService service) {
         this.service = service;
     }
+
     @GetMapping("/")
-    public String goToIndex(){
+    public String goToIndex() {
         return "index";
     }
 
     @GetMapping(value = ALL_USER_URL)
-    public String all(Model model){
+    public String all(Model model) {
         List<User> users = service.getAllUsers();
-        model.addAttribute(USER_LIST_ATTR,users);
+        model.addAttribute(USER_LIST_ATTR, users);
         return USER_PAGE;
 
     }
+
     @GetMapping(value = DELETE_USER_URL)
-    public String delete(int id){
+    public String delete(int id) {
         service.deleteUserById(id);
         return INDEX_PAGE;
 
+    }
+
+    @GetMapping(value = ADD_USER_URL)
+    public ModelAndView add() {
+        ModelAndView modelAndView = new ModelAndView(ADD_USER_PAGE, USER_ATTR, new User());//создается обект который ориентируется на страничку add
+        //и мы туда передаем  аттрибут user, которму установим пустой обект юзера и на этой страничке через кнопку сабмит просетаем ему поля
+        // другой сервлет сохранит уже готового юзера в базу данных
+        return modelAndView;
+
+    }
+
+    @PostMapping(value = SAVE_USER_URL)
+    public String save(@ModelAttribute(name = USER_ATTR) User user) {
+        service.addUser(user);
+        return "redirect:" + ALL_USER_URL;
     }
 }
