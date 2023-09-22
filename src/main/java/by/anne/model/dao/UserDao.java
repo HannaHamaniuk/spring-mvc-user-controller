1f3d264e172f2326088fae2e418167dd6c1dd7c0
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 11:52:01 +0300

    created two interfaces

---
+++ src/main/java/by/anne/model/dao/UserDao.java
@@ -0,0 +1,9 @@
+package by.anne.model.dao;
+
+import by.anne.model.entities.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User>selectAllUsers();
+}
---
+++ src/main/java/by/anne/model/services/UserService.java
@@ -0,0 +1,10 @@
+package by.anne.model.services;
+
+import by.anne.model.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+}
+
