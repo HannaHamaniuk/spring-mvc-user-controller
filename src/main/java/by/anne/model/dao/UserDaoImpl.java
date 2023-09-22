a3d590f56c2f1f27f0e455f40d9c08c4b2630d84
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 12:34:26 +0300

    createn UserDaoImpl

---
+++ src/main/java/by/anne/model/dao/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package by.anne.model.dao;
+
+import by.anne.model.entities.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public List<User> selectAllUsers() {
+        return null;
+    }
+}
