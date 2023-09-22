a77c416f3f9ab58179f99af59b16792bb31fc6ac
HannaHamaniuk <hanna.hamaniuk@gmail.com>
2023-09-22 13:59:22 +0300

    added new constants to AppConstants class

--- src/main/java/by/anne/constants/AppConstants.java
+++ src/main/java/by/anne/constants/AppConstants.java
@@ -2,7 +2,9 @@ package by.anne.constants;
 
 public class AppConstants {
     public static final String ALL_USER_URL = "/all";
+    public static final String DELETE_USER_URL = "/delete";
     public static final String USER_LIST_ATTR = "userList";
     public static final String USER_PAGE = "users";
+    public static final String INDEX_PAGE = "index";
 
 }
